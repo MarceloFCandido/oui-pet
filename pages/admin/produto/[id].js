01cac0e029f752c9260677c8aec60c711dd42630
AllrightIsHere <gasilva912@gmail.com>
2021-12-31 15:19:22 -0300

    feat(add Carousel)

--- pages/admin/produto/[id].js
+++ pages/admin/produto/[id].js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useReducer } from 'react';
+import React, { useEffect, useContext, useReducer, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import NextLink from 'next/link';
@@ -7,6 +7,8 @@ import {
   Button,
   Card,
   CircularProgress,
+  Checkbox,
+  FormControlLabel,
   Grid,
   List,
   ListItem,
@@ -93,6 +95,8 @@ function ProductEdit({ params }) {
           setValue('slug', data.slug);
           setValue('price', data.price);
           setValue('image', data.image);
+          setValue('featuredImage', data.featuredImage);
+          setIsFeatured(data.isFeatured);
           setValue('category', data.category);
           setValue('brand', data.brand);
           setValue('countInStock', data.countInStock);
@@ -105,7 +109,7 @@ function ProductEdit({ params }) {
     }
   }, []);
 
-  const uploadHandler = async (e) => {
+  const uploadHandler = async (e, imageField = 'image') => {
     const file = e.target.files[0];
 
     const bodyFormData = new FormData();
@@ -123,7 +127,7 @@ function ProductEdit({ params }) {
       });
 
       dispatch({ type: 'UPLOAD_SUCCESS' });
-      setValue('image', data.secure_url);
+      setValue(imageField, data.secure_url);
 
       enqueueSnackbar('Arquivo anexado com sucesso', { variant: 'success' });
     } catch (err) {
@@ -138,6 +142,7 @@ function ProductEdit({ params }) {
     price,
     category,
     image,
+    featuredImage,
     brand,
     countInStock,
     description,
@@ -155,6 +160,8 @@ function ProductEdit({ params }) {
           price,
           category,
           image,
+          isFeatured,
+          featuredImage,
           brand,
           countInStock,
           description,
@@ -173,6 +180,9 @@ function ProductEdit({ params }) {
       enqueueSnackbar(getError(err), { variant: 'error' });
     }
   };
+
+  const [isFeatured, setIsFeatured] = useState(false);
+
   return (
     <Layout title={`Editar produto ${productId}`}>
       <Grid container spacing={1}>
@@ -314,6 +324,54 @@ function ProductEdit({ params }) {
                       <Button variant="contained" component="label">
                         Anexar Arquivo
                         <input type="file" onChange={uploadHandler} hidden />
+                      </Button>
+                      {loadingUpload && <CircularProgress />}
+                    </ListItem>
+                    <ListItem>
+                      <FormControlLabel
+                        label="É destaque?"
+                        control={
+                          <Checkbox
+                            onClick={(e) => setIsFeatured(e.target.checked)}
+                            checked={isFeatured}
+                            name="isFeatured"
+                          />
+                        }
+                      ></FormControlLabel>
+                    </ListItem>
+                    <ListItem>
+                      <Controller
+                        name="featuredImage"
+                        control={control}
+                        defaultValue=""
+                        rules={{
+                          required: true,
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            variant="outlined"
+                            fullWidth
+                            id="featuredImage"
+                            label="Imagem em destaque"
+                            error={Boolean(errors.image)}
+                            helperText={
+                              errors.image
+                                ? 'Imagem em destaque é obrigatória'
+                                : ''
+                            }
+                            {...field}
+                          ></TextField>
+                        )}
+                      ></Controller>
+                    </ListItem>
+                    <ListItem>
+                      <Button variant="contained" component="label">
+                        Arquivo em anexo
+                        <input
+                          type="file"
+                          onChange={(e) => uploadHandler(e, 'featuredImage')}
+                          hidden
+                        />
                       </Button>
                       {loadingUpload && <CircularProgress />}
                     </ListItem>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
-import { Grid } from '@mui/material';
+import NextLink from 'next/link';
+import Carousel from 'react-material-ui-carousel';
+import { Grid, Link, Typography } from '@mui/material';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useContext } from 'react';
@@ -7,11 +9,17 @@ import Layout from '../components/Layout';
 import Product from '../models/Product';
 import ProductItem from '../components/ProductItem';
 import db from '../utils/db';
+import useStyles from '../utils/styles';
 
 export default function Home(props) {
+  const classes = useStyles();
+
   const router = useRouter();
+
   const { state, dispatch } = useContext(Store);
-  const { products } = props;
+
+  const { topRatedProducts, featuredProducts } = props;
+
   const addToCartHandler = async (product) => {
     const existItem = state.cart.cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
@@ -25,33 +33,64 @@ export default function Home(props) {
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
     router.push('/carrinho');
   };
+
   return (
     <Layout>
-      <div>
-        <h1>Produtos</h1>
-        <Grid container spacing={3}>
-          {products.map((product) => (
-            <Grid item md={4} key={product.name}>
-              <ProductItem
-                product={product}
-                addToCartHandler={addToCartHandler}
+      <Carousel className={classes.mt1} animation="slide">
+        {featuredProducts.map((product) => (
+          <NextLink
+            key={product._id}
+            href={`/produto/${product.slug}`}
+            passHref
+          >
+            <Link>
+              <img
+                src={product.featuredImage}
+                alt={product.name}
+                className={classes.featuredImage}
               />
-            </Grid>
-          ))}
-        </Grid>
-      </div>
+            </Link>
+          </NextLink>
+        ))}
+      </Carousel>
+      <Typography variant="h2">Popular Products</Typography>
+      <Grid container spacing={3}>
+        {topRatedProducts.map((product) => (
+          <Grid item md={4} key={product.name}>
+            <ProductItem
+              product={product}
+              addToCartHandler={addToCartHandler}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </Layout>
   );
 }
 
 export async function getServerSideProps() {
   await db.connect();
-  const products = await Product.find({}, '-reviews').lean();
+
+  const featuredProductsDocs = await Product.find(
+    { isFeatured: true },
+    '-reviews'
+  )
+    .lean()
+    .limit(3);
+
+  const topRatedProductsDocs = await Product.find({}, '-reviews')
+    .lean()
+    .sort({
+      rating: -1,
+    })
+    .limit(6);
+
   await db.disconnect();
 
   return {
     props: {
-      products: products.map(db.convertDocToObj),
+      featuredProducts: featuredProductsDocs.map(db.convertDocToObj),
+      topRatedProducts: topRatedProductsDocs.map(db.convertDocToObj),
     },
   };
 }
