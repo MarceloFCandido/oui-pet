bbcc1fe7481aed816efaf4cb2aac066352b475f1
MarceloFCandido <fcandido.marcelo@gmail.com>
2022-01-12 14:53:46 -0300

    simplify(route for getting reviews of spcf. user)

--- pages/api/users/reviews/[id].js
+++ pages/api/users/reviews/[id].js
@@ -10,22 +10,18 @@ handler.get(async (req, res) => {
 
   const userId = req.query.id;
 
-  const productsWithReviews = (await Product.find({}))
-    .filter(({ reviews }) => reviews.length)
-    .map(({ name, reviews }) => ({ name, reviews }));
+  const selectedReviews = (await Product.find({}))
+    .map(({ name, reviews }) => ({
+      name,
+      ...reviews
+        .filter(({ user }) => user?.toString() === userId)
+        .map(({ comment, rating, updatedAt }) => ({ comment, rating, updatedAt }))[0]
+    }))
+    .filter(({ updatedAt }) => updatedAt);
 
   await db.disconnect();
 
-  const selectedReviews = productsWithReviews
-    .map(({ name, reviews }) => ({
-      name,
-      reviews: reviews
-        .filter(({ user }) => user.toString() === userId)
-        .map(({ comment, createdAt, rating, updatedAt }) => ({ comment, createdAt, rating, updatedAt }))
-    }))
-    .filter(({ reviews }) => reviews.length);
-
-  res.status(200).send(selectedReviews)
+  res.status(200).send(selectedReviews);
 });
 
 export default handler;
